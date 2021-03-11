2647e9a8ec600e16803ffd9622d62cf6e3b9539b
Keld <keldarch@amalia.anonaddy.com>
2021-03-10 21:51:02 -0500

    added add to cart redux actions and state.

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,14 +1,25 @@
-import React from 'react'
-import {useLocation} from 'react-router-dom';
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { useLocation } from "react-router-dom";
+import { addToCart } from "../actions/cartActions";
 
 export default function CartScreen(props) {
-    const productId = props.match.params.id;
-    const search = useLocation().search;
-    const qty = new URLSearchParams(search).get('qty') || 1;
-    return (
-        <div>
-            <h1>Cart Screen</h1>
-            <p>ADD TO CART: ProductID: {productId} Qty: {qty}</p>
-        </div>
-    )
+  const productId = props.match.params.id;
+  const search = useLocation().search;
+  const qty = new URLSearchParams(search).get("qty") || 1;
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (productId) {
+      dispatch(addToCart(productId, qty));
+    }
+  }, [dispatch, productId, qty]);
+  return (
+    <div>
+      <h1>Cart Screen</h1>
+      <p>
+        ADD TO CART: ProductID: {productId} Qty: {qty}
+      </p>
+    </div>
+  );
 }
