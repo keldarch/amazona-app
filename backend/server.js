6a837f3ea851e1983442bf6cf540d43cf317f183
Keld <keldarch@amalia.anonaddy.com>
2021-03-13 17:58:11 -0500

    added dotenv and jsonwebtoken modules. created user signin api post

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,18 @@
 import express from "express";
 import mongoose from "mongoose";
+import dotenv from "dotenv";
 import productRouter from "./routers/productRouter.js";
 import userRouter from "./routers/userRouter.js";
 
+dotenv.config();
+
 const app = express();
+
+// a middleware to parse json in body of post requests
+app.use(express.json());
+
+// another middleware to parse url encoded form data
+app.use(express.urlencoded({ extended: true }));
 
 mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost/amazona", {
   useNewUrlParser: true,
