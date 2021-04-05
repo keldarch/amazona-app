6a2b6bcb79fe9e1b3d25159c5eb4e86e248cc333
Keld <keldarch@amalia.anonaddy.com>
2021-04-05 19:25:50 -0400

    create and edit products

--- backend/routers/productRouter.js
+++ backend/routers/productRouter.js
@@ -2,6 +2,7 @@ import express from "express";
 import expressAsyncHandler from "express-async-handler";
 import data from "../data.js";
 import Product from "../models/productModel.js";
+import { isAdmin, isAuth } from "../utils.js";
 
 // Router() expands on the routes modularly
 const productRouter = express.Router();
@@ -36,6 +37,50 @@ productRouter.get(
         res.status(404).send({ message: "Product Not Found" });
       }
     } catch (err) {
+      res.status(404).send({ message: "Product Not Found" });
+    }
+  })
+);
+
+productRouter.post(
+  "/",
+  isAuth,
+  isAdmin,
+  expressAsyncHandler(async (req, res) => {
+    const product = new Product({
+      name: "sample name" + Date.now(),
+      image: "/images/p1.jpg",
+      price: 0,
+      category: "sample category",
+      brand: "sample brand",
+      countInStock: 0,
+      rating: 0,
+      numReviews: 0,
+      description: "sample description",
+    });
+    const createdProduct = await product.save();
+    res.send({ message: "Product Created", product: createdProduct });
+  })
+);
+
+productRouter.put(
+  "/:id",
+  isAuth,
+  isAdmin,
+  expressAsyncHandler(async (req, res) => {
+    const productId = req.params.id;
+    const product = await Product.findById(productId);
+    if (product) {
+      product.name = req.body.name || product.name;
+      product.price = req.body.price || product.price;
+      product.image = req.body.image || product.image;
+      product.category = req.body.category || product.category;
+      product.brand = req.body.brand || product.brand;
+      product.countInStock = req.body.countInStock || product.countInStock;
+      product.description = req.body.description || product.description;
+      const updatedProduct = await product.save();
+      res.send({ message: "Product Update", product: updatedProduct });
+    } else {
       res.status(404).send({ message: "Product Not Found" });
     }
   })
