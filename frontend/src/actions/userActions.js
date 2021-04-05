d3b21d44699f38e5644edd5386fa27c9677f681d
Keld <keldarch@amalia.anonaddy.com>
2021-04-05 13:08:29 -0400

    added private route component & functional profile screen

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -10,6 +10,10 @@ import {
   USER_DETAILS_REQUEST,
   USER_DETAILS_SUCCESS,
   USER_DETAILS_FAIL,
+  USER_UPDATE_PROFILE_REQUEST,
+  USER_UPDATE_PROFILE_FAIL,
+  USER_UPDATE_PROFILE_SUCCESS,
+  USER_DETAILS_RESET,
 } from "../constants/userConstants";
 
 export const register = (name, email, password) => async (dispatch) => {
@@ -53,6 +57,7 @@ export const signout = () => (dispatch) => {
   localStorage.removeItem("userInfo");
   localStorage.removeItem("cartItems");
   localStorage.removeItem("shippingAddress");
+  dispatch({ type: USER_DETAILS_RESET });
   dispatch({ type: USER_SIGNOUT });
 };
 
@@ -71,5 +76,26 @@ export const detailsUser = (userId) => async (dispatch, getState) => {
       ? error.response.data.message
       : error.message;
     dispatch({ type: USER_DETAILS_FAIL, payload: message });
+  }
+};
+
+export const updateUserProfile = (user) => async (dispatch, getState) => {
+  dispatch({ type: USER_UPDATE_PROFILE_REQUEST, payload: user });
+  const {
+    userSignin: { userInfo },
+  } = getState();
+  try {
+    const { data } = await axios.put(`/api/users/profile`, user, {
+      headers: { Authorization: `Bearer ${userInfo.token}` },
+    });
+    dispatch({ type: USER_UPDATE_PROFILE_SUCCESS, payload: data });
+    dispatch({ type: USER_DETAILS_SUCCESS, payload: data });
+    dispatch({ type: USER_SIGNIN_SUCCESS, payload: data });
+    localStorage.setItem("userInfo", JSON.stringify(data));
+  } catch (error) {
+    const message = error.response?.data?.message
+      ? error.response.data.message
+      : error.message;
+    dispatch({ type: USER_UPDATE_PROFILE_FAIL, payload: message });
   }
 };
--- frontend/src/screens/ProfileScreen.js
+++ frontend/src/screens/ProfileScreen.js
@@ -1,26 +1,43 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { detailsUser } from "../actions/userActions";
+import { detailsUser, updateUserProfile } from "../actions/userActions";
 import LoadingBox from "../components/LoadingBox";
 import MessageBox from "../components/MessageBox";
+import { USER_UPDATE_PROFILE_RESET } from "../constants/userConstants";
 
 export default function ProfileScreen(props) {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
   const userSignin = useSelector((state) => state.userSignin);
   const { userInfo } = userSignin;
   const userDetails = useSelector((state) => state.userDetails);
   const { loading, error, user } = userDetails;
+  const userUpdateProfile = useSelector((state) => state.userUpdateProfile);
+  const {
+    success: successUpdate,
+    error: errorUpdate,
+    loading: loadingUpdate,
+  } = userUpdateProfile;
   const dispatch = useDispatch();
   useEffect(() => {
-    if (!userInfo) {
-      props.history.push(`/signin`);
-    }
-    if (userInfo) {
+    if (!user) {
+      dispatch({ type: USER_UPDATE_PROFILE_RESET });
       dispatch(detailsUser(userInfo._id));
+    } else {
+      setName(user.name);
+      setEmail(user.email);
     }
-  }, [dispatch, props.history, userInfo]);
+  }, [dispatch, userInfo._id, user]);
   const submitHandler = (e) => {
     e.preventDefault();
-    // dispatch update profile
+    if (password !== confirmPassword) {
+      alert("Password and Confirm Password do not match");
+    } else {
+      dispatch(updateUserProfile({ userId: user._id, name, email, password }));
+    }
   };
   return (
     <div>
@@ -34,13 +51,23 @@ export default function ProfileScreen(props) {
           <MessageBox variant="danger">{error}</MessageBox>
         ) : (
           <>
+            {loadingUpdate && <LoadingBox></LoadingBox>}
+            {errorUpdate && (
+              <MessageBox variant="danger">{errorUpdate}</MessageBox>
+            )}
+            {successUpdate && (
+              <MessageBox variant="success">
+                Profile Update Successfully
+              </MessageBox>
+            )}
             <div>
               <label htmlFor="name">Name</label>
               <input
                 type="text"
                 id="name"
                 placeholder="Enter name"
-                value={user.name}
+                value={name}
+                onChange={(e) => setName(e.target.value)}
               />
             </div>
             <div>
@@ -49,12 +76,18 @@ export default function ProfileScreen(props) {
                 type="text"
                 id="email"
                 placeholder="Enter email"
-                value={user.email}
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
               />
             </div>
             <div>
               <label htmlFor="password">Password</label>
-              <input type="text" id="password" placeholder="Enter password" />
+              <input
+                type="text"
+                id="password"
+                placeholder="Enter password"
+                onChange={(e) => setPassword(e.target.value)}
+              />
             </div>
             <div>
               <label htmlFor="confirmPassword">Confirm Password</label>
@@ -62,6 +95,7 @@ export default function ProfileScreen(props) {
                 type="text"
                 id="confirmPassword"
                 placeholder="Enter confirm password"
+                onChange={(e) => setConfirmPassword(e.target.value)}
               />
             </div>
             <div>
