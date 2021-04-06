ed5733f9f5828d90e110c7ef633355cde28bd8c7
Keld <keldarch@amalia.anonaddy.com>
2021-04-06 14:44:34 -0400

    publish to heroku v4

--- backend/server.js
+++ backend/server.js
@@ -34,7 +34,7 @@ const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
 // for production build (with heroku)
-app.use("/uploads", express.static(path.join(__dirname, "/frontend/build")));
+app.use(express.static(path.join(__dirname, "/frontend/build")));
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "/frontend/build/index.html"));
 });
